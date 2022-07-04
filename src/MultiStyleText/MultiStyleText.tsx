a97850e84641e49523f89e7922a5934923f30d30
qiuli-li <11270132+qiuli-li@user.noreply.gitee.com>
2022-07-04 15:59:18 +0800

    fix: 🐛 复制时样式换行消失

--- src/MultiStyleText/MultiStyleText.tsx
+++ src/MultiStyleText/MultiStyleText.tsx
@@ -5,9 +5,12 @@ import ContentEditable from 'react-contenteditable'
 import styles from './MultiStyleText.module.less'
 import { memo, useEffect, useRef, useState } from 'react'
 
-import sanitizeHtml from 'sanitize-html'
 import { MultiStyleTextProps } from '../type/Style'
-import { insertHtmlAtSelectionEnd } from './MultiStyleTextMethod'
+import {
+  checkHtml,
+  handleAfterPaste,
+  insertHtmlAtSelectionEnd
+} from './MultiStyleTextMethod'
 
 export const MultiStyleText = memo(
   ({
@@ -22,14 +25,11 @@ export const MultiStyleText = memo(
       getHeight
     }
   }: MultiStyleTextProps) => {
+    const [texts, setText] = useState({ html: content })
     const text: { current?: string } = useRef(content)
     const [CurrentStyle, setCurrentStyle] = useState(textStyle)
     const [Editable, setEditable] = useState(editable)
     const [CurrentSpace, setCurrentSpace] = useState(textSpace)
-    // const [Content, setContent] = useState(content)
-    // const sanitizeConf = {
-    //     allowedTags: []
-    // }
 
     const init =
       CurrentStyle === 'ep-blocks'
@@ -45,10 +45,6 @@ export const MultiStyleText = memo(
       ...init
     }
 
-    const checkHtml = (htmlStr?: string) => {
-      const reg = /<[^>]+>/g
-      return reg.test(htmlStr || '')
-    }
     useEffect(() => {
       setCurrentStyle(textStyle)
     }, [textStyle])
@@ -62,10 +58,6 @@ export const MultiStyleText = memo(
       if (getMultiText && editable) getMultiText(text.current || '')
     }, [editable])
 
-    // useEffect(() => {
-    //     setContent(content)
-    // }, [content])
-
     useEffect(() => {
       if (!checkHtml(text.current)) {
         text.current = `<div>${text.current}</div>`
@@ -77,103 +69,37 @@ export const MultiStyleText = memo(
       target: { value: string }
       currentTarget: { clientHeight: number }
     }) => {
-      if (getHeight) getHeight({ height: evt.currentTarget.clientHeight })
-      const cleanHtml = sanitizeHtml(evt.target.value, {
-        allowedTags: ['div', 'br']
-      })
-      if (!checkHtml(cleanHtml)) {
-        text.current = `<div>${cleanHtml || '<br>'}</div>`
-        return
-      }
       if (
-        cleanHtml === '<div><br></div>' ||
-        cleanHtml === '<br>' ||
-        cleanHtml === ''
+        !evt.currentTarget.innerHTML ||
+        evt.currentTarget.innerHTML === '<br>'
       ) {
-        text.current = '<div><br></div>'
-        return
+        setText({ html: '<div><br></div>' })
       }
-      text.current = cleanHtml
-      if (getMultiText) getMultiText(text.current || '')
+      if (getHeight) getHeight({ height: evt.currentTarget.clientHeight })
+      if (getMultiText) getMultiText(evt.currentTarget?.innerHTML || '')
     }
-    // const handleBlur = () => {
-    //   const clean = sanitizeHtml(text.current || '', sanitizeConf)
-    // console.log(666666)
-    // }
-    // const handleFocus = () => {
-    //     console.log(444444, text.current);
-    // }
-    // const handleKeyUp = (e) => {
-    //     // debugger
-    //     // console.log(55555, e);
-    // }
-
-    // const handleKeyDown = () => {
-    //     // debugger
-    //     // console.log(666666, text.current);
-    // }
-
-    // const handlePaste = (e) => {
-    //     debugger
-    //     type = 'paste'
-    // }
     const handlePaste = (ev: React.ClipboardEvent<HTMLDivElement>) => {
-      console.log(
-        999999,
-        ev.clipboardData.getData('text/html'),
-        ev.clipboardData.getData('text'),
-        ev.clipboardData.getData('html')
-      )
-      // const cleanHtml = sanitizeHtml(
-      //     ev.clipboardData.getData('text/html'),
-      //     sanitizeConf
-      // )
       const cleanHtml = ev.clipboardData.getData('text')
       ev.clipboardData.setData('text/html', cleanHtml)
       insertHtmlAtSelectionEnd(cleanHtml, false)
+
+      handleAfterPaste(ev)
       ev.preventDefault()
-      //   text.current = ev.currentTarget.innerHTML
-      if (getMultiText) getMultiText(text.current || '')
       if (getHeight) getHeight({ height: ev.currentTarget.clientHeight })
     }
-    // const handlePastePasteCapture = (e) => {
-    //     // debugger
-    //     // console.log(1010110, e, text.current);
-    // }
-    // const onCompositionEnd = (e) => {
-    //     debugger
-    //     // console.log(112121212, e, text.current);
-    // }
-    // const onLoadedData = (e) => {
-    //     debugger
-    //     // console.log(13131413, e, text.current);
-    // }
-    // const handleChangeCapture = (e) => {
-    //     debugger
-    //     // console.log(13131413, e, text.current);
-    // }
+    console.log(texts.html)
 
     return (
       <ContentEditable
-        // innerRef={text}
         onPaste={handlePaste}
-        // onPasteCapture={handlePastePasteCapture}
-        // onCompositionEnd={onCompositionEnd}
-        // onLoadedData={onLoadedData}
-        // onBlur={handleBlur}
-        // onFocus={handleFocus}
-        // onKeyUp={handleKeyUp}
-        // onKeyDown={handleKeyDown}
         className={`editable ${className} ${styles[CurrentStyle || '']} ${
           styles[CurrentSpace || '']
         }`}
         tagName='div'
         style={{ ...ContentEditableDefauleStyle, ...editorstyle }}
-        html={text.current || ''}
-        // html={Content || ''}
+        html={texts.html || '<div><br></div>'}
         disabled={!Editable}
         onChange={handleChange}
-        // onChangeCapture={handleChangeCapture}
       />
     )
   }
--- src/MultiStyleText/MultiStyleTextMethod.ts
+++ src/MultiStyleText/MultiStyleTextMethod.ts
@@ -15,6 +15,7 @@ export const insertHtmlAtSelectionEnd = (html: string, isBefore: boolean) => {
         frag.appendChild(node)
       }
       range && range.insertNode(frag)
+      sel.addRange(range)
     }
   } else if (
     (document as any).selection &&
@@ -24,4 +25,62 @@ export const insertHtmlAtSelectionEnd = (html: string, isBefore: boolean) => {
     range.collapse(isBefore)
     range.pasteHTML(html)
   }
+}
+
+export const Ize = (str) => {
+  const parse = new DOMParser()
+  const doc = parse.parseFromString(str, 'text/html')
+  return doc.body.innerHTML
+}
+
+export const handleAfterPaste = (ev) => {
+  const CurrentDom = ev.currentTarget
+
+  const CleanDiv = /<div>/g
+  const ReplaceEndDiv = /<\/div>/g
+
+  const StandardString = ev.currentTarget.innerHTML
+    .replace(CleanDiv, '')
+    .replace(ReplaceEndDiv, '\n')
+
+  const regex = /(\r)?\n/gi
+  const NullDiv = /<div><\/div>/g
+
+  let StrHtml = StandardString.replace(regex, '</div><div>')
+
+  if (!StrHtml.startsWith('<div>') && !StrHtml.endsWith('</div>')) {
+    StrHtml = `<div>${StrHtml}</div>`
+  }
+  StrHtml = StrHtml.replace(NullDiv, '')
+
+  let sel: any
+  if (window.getSelection) {
+    sel = window.getSelection()
+    const last = sel?.getRangeAt(0)
+    const StartContent = CurrentDom.firstChild
+    const EndContent = CurrentDom.lastChild
+    if (sel.getRangeAt && sel.rangeCount) {
+      const range = document.createRange()
+      const Html = Ize(StrHtml)
+      const el = document.createElement('div')
+      el.innerHTML = Html
+
+      range && range.setStartBefore(StartContent)
+      range && range.setEndAfter(EndContent)
+      range && range.deleteContents()
+
+      const frag = document.createDocumentFragment()
+      let node
+      while ((node = el.firstChild)) {
+        frag.appendChild(node)
+      }
+      range && range.insertNode(frag)
+      sel.addRange(last)
+    }
+  }
+}
+
+export const checkHtml = (htmlStr?: string) => {
+  const reg = /<[^>]+>/g
+  return reg.test(htmlStr || '')
 }
